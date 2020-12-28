31da5c12d4b7825e9c2d7c05b47fec2b91035ef8
rwu <ruichao.wu@ipa.fraunhofer.de>
2020-12-28 21:16:25 +0100

    add bringuup

---
+++ minion_bringup/launch/tank_hardware.launch.py
@@ -0,0 +1,55 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    usb_port = LaunchConfiguration('usb_port', default='/dev/ttyUSB0')
+
+    lidar_pkg_dir = LaunchConfiguration(
+        'lidar_pkg_dir',
+        default=os.path.join(get_package_share_directory('rplidar_ros'), 'launch'))
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='Use simulation (Gazebo) clock if true'),
+
+        DeclareLaunchArgument(
+            'usb_port',
+            default_value=usb_port,
+            description='Connected USB port with Lidar'),
+
+        # DeclareLaunchArgument(
+        #     'tb3_param_dir',
+        #     default_value=tb3_param_dir,
+        #     description='Full path to turtlebot3 parameter file to load'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [ThisLaunchFileDir(), '/tank_driver_launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([lidar_pkg_dir, '/rplidar.launch.py']),
+            launch_arguments={'port': '/dev/ttyUSB0', 'frame_id': 'base_scan'}.items(),
+        ),
+
+        Node(
+            package='minion_bringup',
+            executable='minion_ros2',
+            # parameters=[tb3_param_dir],
+            # arguments=['-i', usb_port],
+            output='screen'),
+    ])
