09ea6ffa4865b7425851d9d03ffb6831ea502bcd
rwu <ruichao.wu@ipa.fraunhofer.de>
2020-12-31 15:43:38 +0100

    fix

--- tank_driver_ros2/tank_driver_ros2/imu_publisher.py
+++ tank_driver_ros2/tank_driver_ros2/imu_publisher.py
@@ -49,7 +49,7 @@ class ImuPublisherNode(Node):
                                 'y': _gyro_offset_y,
                                 'z': _gyro_offset_z
                                 }
-        self.get_logger().info('offset accel: %f, %f, %f'% self.self._setAccelOffset['x'] % self.self._setAccelOffset['y']% self.self._setAccelOffset['z'])
+        self.get_logger().info('offset accel: %f, %f, %f'%self._setAccelOffset['x'] %self._setAccelOffset['y']% self._setAccelOffset['z'])
 
         # Setup publisher for imu message
         self.publisher_imu = self.create_publisher(Imu, self._imu_topic, 10)
